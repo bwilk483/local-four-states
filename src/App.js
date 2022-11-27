64c3fc3e518bade067e90bd3ca640b047dc4f8a9
Bryan Wilkerson <bwilk483@gmail.com>
2022-11-27 17:36:06 -0600

    made blog page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./routes/Home/Home";
 import LocalBusinesses from "./routes/Local/Local";
 import About from "./routes/About/About";
 import Contact from "./routes/Contact/Contact";
+import Blog from "./routes/Blog/Blog";
 
 import { Route, Routes } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/LocalBusinesses" element={<LocalBusinesses />}></Route>
+        <Route path="/blog" element={<Blog />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
