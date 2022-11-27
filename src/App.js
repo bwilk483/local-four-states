0d449e7c401d9e13e7ae85a8923e891b11cd0155
Bryan Wilkerson <bwilk483@gmail.com>
2022-11-27 17:34:14 -0600

    made contact page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./app.css";
 import Home from "./routes/Home/Home";
 import LocalBusinesses from "./routes/Local/Local";
 import About from "./routes/About/About";
+import Contact from "./routes/Contact/Contact";
 
 import { Route, Routes } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/LocalBusinesses" element={<LocalBusinesses />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </>
   );
